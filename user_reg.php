873a249c0c3031a83d0b835d24f3099d502c70e8
Jamil Ahmad <31651829+jamilahmadicp@users.noreply.github.com>
2021-10-27 14:56:09 +0500

    Update user_reg.php

--- user_reg.php
+++ user_reg.php
@@ -1,41 +1,42 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "geniusdb";
+$username = "id17794283_jamil";
+$password = "AcerPredator1@";
+$dbname = "id17794283_geniusdb";
 
 $con = new mysqli($servername, $username, $password, $dbname);
 
 if($con->connect_error)
 {
     // echo $con->connect_error;
-    die(False);
+    //die(False);
+    echo json_encode(array("success"=>False));
 }
 
 
 // Receive and decode JSON data
-$data = $_POST["user_data"];
-$dec_data = json_decode($data);
+//$data = $_POST["user_data"];
+//$dec_data = json_decode($data);
 
-$uname = $dec_data->uname;
-$user_name = $dec_data->user_name;
-$password = $dec_data->password;
-$gender = $dec_data->gender;
-$grade = $dec_data->grade;
-$role = $dec_data->role;
-$institute = $dec_data->institute;
-$contact = $dec_data->contact;
+$uname = $_POST["uname"];
+$user_name = $_POST["user_name"];
+$password = $_POST["password"];
+$gender = $_POST["gender"];
+$grade = $_POST["grade"];
+$role = $_POST["role"];
+$institute = $_POST["institute"];
+$contact = $_POST["contact"];
 
 
 $sql = "insert into users(uname, user_name, password, gender, grade, role, institute, contact) values('$uname', '$user_name', '$password', '$gender', '$grade', '$role', '$institute', '$contact')";
 
 if($con->query($sql)==TRUE)
 {
-    echo True;
+    echo json_encode(array("success"=>True));
 }
 else
 {
-    echo "error: ". $sql." <br>". $con->error;
+    echo json_encode(array("success"=>False));
 }
 
  $con->close();
